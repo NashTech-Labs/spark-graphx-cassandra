1c6d8fcfe9409b2ea371875637765524b536370c
Himanshu Gupta <himanshu@knoldus.com>
2016-08-19 14:19:39 +0530

    #3: Create VertexProperty to model a Property Graph in GraphX

--- src/main/scala/com/knoldus/spark/graphx/PropertyGraph.scala
+++ src/main/scala/com/knoldus/spark/graphx/PropertyGraph.scala
@@ -2,6 +2,10 @@ package com.knoldus.spark.graphx
 
 import org.apache.spark.SparkContext
 
+class VertexProperty()
+case class UserProperty(val name: String) extends VertexProperty
+case class ProductProperty(val name: String, val price: Double) extends VertexProperty
+
 class PropertyGraph(sparkContext: SparkContext) {
 
 }
