f1f6e116b47c249a950d611dec23fcc1cb8e1c25
Himanshu Gupta <himanshu@knoldus.com>
2016-08-19 15:20:57 +0530

    #3: created a triplet view creator of graph

--- src/main/scala/com/knoldus/spark/graphx/PropertyGraph.scala
+++ src/main/scala/com/knoldus/spark/graphx/PropertyGraph.scala
@@ -7,4 +7,7 @@ import org.apache.spark.rdd.RDD
 class PropertyGraph(sparkContext: SparkContext) {
   def getGraph(vertices: RDD[(VertexId, (String, String))], edges: RDD[Edge[String]], defaultVertexAttr: (String, String)): Graph[(String, String), String] =
     Graph(vertices, edges, defaultVertexAttr)
+
+  def getTripletView(graph: Graph[(String, String), String]): RDD[String] =
+    graph.triplets.map(triplet => s"${triplet.srcAttr} is the ${triplet.attr} of ${triplet.dstAttr}")
 }
--- src/test/scala/com/knoldus/spark/graphx/PropertyGraphTest.scala
+++ src/test/scala/com/knoldus/spark/graphx/PropertyGraphTest.scala
@@ -1,26 +1,32 @@
 package com.knoldus.spark.graphx
 
-import org.apache.spark.graphx.{ Edge, VertexId }
+import org.apache.spark.graphx.{ Edge, Graph, VertexId }
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{ SparkConf, SparkContext }
 import org.scalatest.FunSuite
 
 object TestData {
   val sparkContext = new SparkContext(new SparkConf().setMaster("local").setAppName("test"))
+
+  val users: RDD[(VertexId, (String, String))] =
+    sparkContext.parallelize(Array((3L, ("rxin", "student")), (7L, ("jgonzal", "postdoc")), (5L, ("franklin", "prof")), (2L, ("istoica", "prof"))))
+  val relationships: RDD[Edge[String]] =
+    sparkContext.parallelize(Array(Edge(3L, 7L, "collab"), Edge(5L, 3L, "advisor"), Edge(2L, 5L, "colleague"), Edge(5L, 7L, "pi")))
+  val defaultUser = ("John Doe", "Missing")
 }
 
 class PropertyGraphTest extends FunSuite {
-  import com.knoldus.spark.graphx.TestData.sparkContext
+  import com.knoldus.spark.graphx.TestData._
 
   val propertyGraph = new PropertyGraph(sparkContext)
 
   test("property graph returns graph") {
-    val users: RDD[(VertexId, (String, String))] =
-      sparkContext.parallelize(Array((3L, ("rxin", "student")), (7L, ("jgonzal", "postdoc")), (5L, ("franklin", "prof")), (2L, ("istoica", "prof"))))
-    val relationships: RDD[Edge[String]] =
-      sparkContext.parallelize(Array(Edge(3L, 7L, "collab"), Edge(5L, 3L, "advisor"), Edge(2L, 5L, "colleague"), Edge(5L, 7L, "pi")))
-    val defaultUser = ("John Doe", "Missing")
     val graph = propertyGraph.getGraph(users, relationships, defaultUser)
     assert(graph.edges.count() === 4)
+  }
+
+  test("property graph returns triplets in a graph") {
+    val graph = propertyGraph.getTripletView(Graph(users, relationships, defaultUser))
+    assert(graph.count() === 4)
   }
 }
